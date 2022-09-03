7c9518917d7a302e23f764db940f6691b421b62b
dhvcc <1337kwiz@gmail.com>
2022-09-03 06:07:32 +0300

    Add redux, toolkit and the wrapper

---
+++ src/redux/store.ts
@@ -0,0 +1,45 @@
+import {
+  Action,
+  AnyAction,
+  combineReducers,
+  configureStore,
+  ThunkAction,
+} from '@reduxjs/toolkit';
+import {createWrapper, HYDRATE} from 'next-redux-wrapper';
+
+const combinedReducer = combineReducers({
+  blank: function(state, action) {
+    if (state == null) state = [];
+    return state;
+  },
+});
+
+const reducer = (
+    state: ReturnType<typeof combinedReducer>, action: AnyAction) => {
+  if (action.type === HYDRATE) {
+    return {
+      ...state, // use previous state
+      ...action.payload, // apply delta from hydration
+    };
+  } else {
+    return combinedReducer(state, action);
+  }
+};
+
+export const makeStore = () =>
+    configureStore({
+      // @ts-ignore
+      reducer,
+    });
+
+type Store = ReturnType<typeof makeStore>;
+
+export type AppDispatch = Store['dispatch'];
+export type RootState = ReturnType<Store['getState']>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
+    RootState,
+    unknown,
+    Action<string>>;
+
+export const wrapper = createWrapper(makeStore,
+    {debug: process.env.NODE_ENV === 'development'});
