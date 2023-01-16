8333d609008aea2e7a5c9286a5146e16837d59e0
Alexey <1337kwiz@gmail.com>
2023-01-16 18:17:46 +0300

    Add NEXTAUTH_URL to production .env
    Remove temp ui from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,14 @@
 import type { NextPage } from 'next';
-import Button from '@mui/joy/Button';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { useSearchQuery } from "../core/api/mangaApi";
+import { MangaGrid } from "../components/manga/grid/MangaGrid";
 
 const Home: NextPage = () => {
-  // const { data } = useSearchQuery('бибоп');
-  const { data: session, status } = useSession();
+  const { data } = useSearchQuery('бибоп');
 
   return (
     <>
       <h1>Sora Reader</h1>
-      <p>Is auth {status}</p>
-      <p style={{ wordBreak: 'break-word' }}>{JSON.stringify(session)}</p>
-      <Button onClick={() => signIn('credentials', { username: 'r', password: 'r' })}>SignIn</Button>
-      <Button onClick={() => signOut()}>SignOut</Button>
-      {/*{data && <MangaGrid mangaList={data.slice(0, 4)} loading={false} />}*/}
+      {data && <MangaGrid mangaList={data.slice(0, 4)} loading={false} />}
     </>
   );
 };
